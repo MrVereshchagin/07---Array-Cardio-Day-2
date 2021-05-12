deda3400db28e6d3ca9376350be7bddfdbd2fbff
Igor Vereshchagin <9868425@gmail.com>
2021-05-12 23:19:48 +0300

    feat: finish lesson

--- script.js
+++ script.js
@@ -34,7 +34,21 @@ const people = [
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
+  const findId = comments.find(phrase => phrase.id === 823423);
+
+  console.log(findId);
 
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
+  const findIndexOfId = comments.findIndex(phrase => phrase.id === 823423);
+
+  console.log(findIndexOfId);
+
+  // comments.splice(findIndexOfId, 1);
+
+  const newComments = [
+    ...comments.slice(0, findIndexOfId),
+    ...comments.slice(findIndexOfId + 1)
+  ]; // этот момент не понял, что он делает? Создает новый массив?
+
